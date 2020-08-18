1fc2c4e7d957e46d514dfaab133f93462b58d179
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-18 14:42:41 -0700

    [ADD] route for getting date intervals

--- public/script.js
+++ public/script.js
@@ -2,17 +2,30 @@
 // A basic API call which shows that a simulation can be created using these API routes. 
 // Run the server in the server.js file and open localhost:3000 to see the result in the console
 
-$.ajax({
-    method: "POST",
-    url: "/api/simulation/new",
-    data: {
-        symbol: 'SPY',
-        startDate: "2018-10-01",
-        endDate: "2020-08-01",
-        investment: 10000,
-        strategyFuncName: "monthlyInvestment",
-        strategyParams: [1000]
-    }
-}).then(result => {
-    console.log(result)
-})
+function runStrategy(symbol, startDate, endDate, interval) {
+    const object = { symbol: symbol, startDate: startDate, endDate: endDate, interval: interval };
+    $.ajax({
+        method: "POST",
+        url: "/api/simulation/getIntervalDates",
+        data: object
+    })
+        .then(actionDates => {
+            console.log("action dates is: " + actionDates)
+            $.ajax({
+                method: "POST",
+                url: "/api/simulation/new",
+                data: {
+                    symbol: symbol,
+                    startDate: startDate,
+                    endDate: endDate,
+                    investment: 100,
+                    strategyFuncName: "monthlyInvestment",
+                    strategyParams: [10, actionDates]
+                }
+            }).then(result => {
+                console.log(result)
+            })
+        })
+}
+
+runStrategy("SPY", "2018-08-01", "2020-08-01", 10)
--- simulations/strategyFuncs/monthlyInvestment.js
+++ simulations/strategyFuncs/monthlyInvestment.js
@@ -1,39 +1,24 @@
 // buys the given stock whenever you have enough cash and increments your cash by a given amount after every 20 market days
-
 const Suggestion = require("./utils/Suggestion")
 
-function monthlyInvestment(monthlyAmount, symbol, portfolio, stockData, currentDate) {
+function monthlyInvestment(monthlyAmount, actionDates, symbol, portfolio, stockData, currentDate) {
 
     const suggestions = []
 
-    if (typeof dayCounter === "undefined") {
-        dayCounter = 0;
-        investmentCounter = 0;
-    }
-
-    if (dayCounter === 20) {
+    if (actionDates.includes(currentDate)) {
         console.log(`investing another $${monthlyAmount} into the portfolio`);
         portfolio.increaseCash(monthlyAmount)
-        dayCounter = 0;
-        investmentCounter += 1;
-    } else {
-        dayCounter += 1;
     }
 
     var cashCopy = portfolio.getCash
     console.log(`Date: ${currentDate}, Portfolio: ${portfolio.totalValue}`)
+    
 
     while (stockData[currentDate]["markPrice"] < cashCopy) {
         console.log("Suggesting to buy another share")
         suggestions.push(Suggestion.createBuySuggestion(symbol, stockData, currentDate))
         cashCopy -= stockData[currentDate]["markPrice"]
     }
-
-    var moneyInvested = investmentCounter * monthlyAmount;
-    console.log(`money you've invested: ${moneyInvested}`)
-
-    var profit = portfolio.totalValue - 10000 - moneyInvested;
-    console.log(`total profit: ${profit}`)
 
     return suggestions
 
