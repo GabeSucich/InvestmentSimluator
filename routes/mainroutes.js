85fb63c5d160ced405c88444df0fb1034dd78b74
Gabe Sucich <gabe.sucich@gmail.com>
2020-08-18 15:20:54 -0700

    Changing API route so that for find interval will grab data from database if available

--- routes/mainroutes.js
+++ routes/mainroutes.js
@@ -3,7 +3,12 @@ var path = require("path")
 const API = require("../simulations/utils/API")
 const Utils = require("../simulations/utils/dateUtils")
 
-module.exports = function(app) {
+const mongoose = require("mongoose")
+const StockHistory = require("../models/StockHistory")
+
+mongoose.connect(process.MONGODB_URI || "mongodb://localhost/stockhistorydb", { useNewUrlParser: true })
+
+module.exports = function (app) {
 
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "/public/index.html"))
@@ -12,24 +17,36 @@ module.exports = function(app) {
     // This is a basic api call for simulation data. In the request comes the information needed to create a simulation. 
     //The simulation is instantiated and run, and the data is sent back to the client
     app.post('/api/simulation/new', (req, res) => {
-    const {symbol, startDate, endDate, investment, strategyFuncName, strategyParams} = req.body
+        const { symbol, startDate, endDate, investment, strategyFuncName, strategyParams } = req.body
         const simControl = new SimControl(symbol, startDate, endDate, investment, strategyFuncName, strategyParams)
         simControl.runSimulation().then(data => {
             setTimeout(() => {
-                res.json(simControl.simulationResult)    
+                res.json(simControl.simulationResult)
             }, 2000)
-            
-            
+
+
         })
     })
 
     app.post("/api/simulation/getIntervalDates", (req, res) => {
-        const {symbol, startDate, endDate, interval} = req.body
-        API.getStockData(symbol)
-        .then(response => {
-            const historicals = response.data["Time Series (Daily)"]
-            const resultDates = Utils.findIntervalDates(historicals, startDate, endDate, interval)
-            res.json(resultDates);
+        const { symbol, startDate, endDate, interval } = req.body
+
+        StockHistory.findOne({ symbol: symbol }).exec().then(databaseData => {
+            if (!databaseData) {
+                API.getStockData(symbol)
+                    .then(response => {
+                        const historicals = response.data["Time Series (Daily)"]
+                        const resultDates = Utils.findIntervalDates(historicals, startDate, endDate, interval)
+                        res.json(resultDates);
+                    })
+            }
+            else {
+                console.log("Grabbing from database")
+                const historicals = databaseData.historicals
+                const resultDates = Utils.findIntervalDates(historicals, startDate, endDate, interval)
+                res.json(resultDates)
+            }
+
         })
     })
 
