2097e24284f9cd586029ad1d692e1a353ba57a83
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-19 12:59:40 -0700

    added ChartHandler and Chart React components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from "axios"
+import ChartHandler from "./components/ChartHandler"
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  Axios({
+    method: "POST",
+    url: "/api/simulation/getIntervalDates",
+    data: {
+      symbol: "SPY",
+      startDate: "2010-08-10",
+      endDate: "2020-08-10",
+      interval: 300
+    }
+  })
+    .then(actionDates => {
+      Axios({
+        method: "POST",
+        url: "/api/simulation/new",
+        data: {
+          symbol: "SPY",
+          startDate: "2010-08-10",
+          endDate: "2020-08-10",
+          investment: 1000,
+          strategyFuncName: "monthlyInvestment",
+          strategyParams: [100, actionDates]
+        }
+      }).then(result => {
+        setData([...data, result])
+        console.log(result)
+      })
+    })
+
   return (
-    
+    <div>
+      data.length > 0 ? <ChartHandler simulations={data} labels={["test"]} />
+    </div>
   );
 }
 
