00a236808016528803e09c6772362592f3b315b7
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-20 14:19:19 -0700

    [ADD] route to run multiple simulations

--- client/src/components/ChartHandler/index.js
+++ client/src/components/ChartHandler/index.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-import { Line } from 'react-chartjs-2';
-import React from "react"
-
-export default function ChartHandler(props) {
-    const { simulations } = props; // simulations is an array of objects, each object has a key of the simulation pointing to that simulation's data
-    console.log(simulations)
-    const { labels } = props; // the names of the series to be charted
-    if (typeof(props.options) === "undefined") {
-        var options = {}
-    } else {
-        var { options } = props;
-    }
-
-    function processSimulation(i) {
-        var simulation = simulations[i];
-        var label = labels[i];
-        var data = [];
-        console.log(simulation)
-        for (const dataPoint of simulation.portfolioHistory) {
-            data.push(eval(dataPoint.totalValue))
-        }
-        return {"label": label, "data": data}
-    }
-
-    function processAllSimulations(simulations) {
-        const datasets = [];
-        for (var i in simulations) {
-            var processedSimulation = processSimulation(i);
-            datasets.push(processedSimulation);
-        }
-        return {"labels": processDates(simulations), "datasets": datasets}
-    }
-
-    function processDates(simulations) {
-        var simulation = simulations[0];
-        var dates = [];
-        for (const data of simulation.portfolioHistory) {
-            dates.push(data.date)
-        }
-        return dates;
-    }
-
-    return (
-        <div className="container">
-            <Line data={processAllSimulations(simulations)} options={options} />
-        </div>
-    )
-=======
 import { Line } from 'react-chartjs-2';
 import React from "react"
 
@@ -104,5 +55,4 @@ export default function ChartHandler(props) {
             <Line data={processAllSimulations(simulations)} options={options} />
         </div>
     )
->>>>>>> f40e7dd187954556d952d72eff2af1f11dbf900b
 }
