c87c8b36801c2b00c383e58bde3eb794bd69523e
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-24 10:45:16 -0700

    fixed syntax error in public/script.js

--- public/script.js
+++ public/script.js
@@ -2,19 +2,19 @@
 // A basic API call which shows that a simulation can be created using these API routes. 
 // Run the server in the server.js file and open localhost:3000 to see the result in the console
 
-$.ajax({
-    method: "POST",
-    url: "/api/joel",
-    data: {
-        symbol: 'AAPL',
-        startDate: "2005-01-03",
-        endDate: "2020-06-01",
-        investment: 10000,
-        strategyFuncName: "stateTaxAffect",
-        strategyParams: [13, 0]
-    }
-}).then(result => {
-    console.log(result)
+// $.ajax({
+//     method: "POST",
+//     url: "/api/joel",
+//     data: {
+//         symbol: 'AAPL',
+//         startDate: "2005-01-03",
+//         endDate: "2020-06-01",
+//         investment: 10000,
+//         strategyFuncName: "stateTaxAffect",
+//         strategyParams: [13, 0]
+//     }
+// }).then(result => {
+//     console.log(result)
 
 //     // logic goes here
 //     let percentStateTax1 = stateTaxRate1 / 100
@@ -45,6 +45,6 @@ $.ajax({
 //                 console.log(result)
 //             })
 //         })
-}
+// }
 
 // runStrategy("ACB", "2010-08-01", "2020-08-01", 10)
