47375283cd44bd1162c6e258174e9c1f21925023
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-27 10:35:06 -0700

    [updated] actTrade helper.

--- client/src/pages/ActiveTrading/utils/Helper.js
+++ client/src/pages/ActiveTrading/utils/Helper.js
@@ -31,7 +31,38 @@ const Helper = {
             console.log('helper, passes first tests ' + str);
             return this.process(str);
         }
-    }
+    },
+
+    findFirstDateInYear(historicals, year) {
+        console.log(year)
+        for (const [date, data] of Object.entries(historicals)) {
+            if (date.slice(0,4) === year) {
+                return date
+            }
+        }
+        return null
+    },
+
+    findLastDateInYear(historicals, year) {
+        const dates = Object.keys(historicals)
+        var foundYear = false
+        var previousDate;
+        for (const date of dates) {
+
+            var thisYear = date.slice(0,4)
+            if (thisYear === year) {
+                foundYear = false
+            }
+            else if (thisYear !== year && foundYear) {
+                return previousDate
+            }
+            previousDate = date
+        }
+
+        return previousDate
+
+    },
+
 }
 
 export default Helper
