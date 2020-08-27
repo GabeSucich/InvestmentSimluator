8147f8e3bce2d070b64120418fdbe91996289ffc
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-27 15:10:26 -0700

    [debug] ActTrade.

--- client/src/pages/ActiveTrading/index.js
+++ client/src/pages/ActiveTrading/index.js
@@ -1,111 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import { StandardForm } from "../../SemanticUI/Forms/index"
-import { useActiveTradingContext } from './utils/ActiveState'
-import Helper from "./utils/Helper"
-// import API from "../../../../../utils/API"
-import { SET_PARAMS, CLEAR_DATA, } from "./utils/activeAction"
-import { SET_SIMULATION_DATA } from '../GatherInformation/utils/action'
-// import "./style.css"
-import API from '../../../src/utils/API'
-import { Segment, Input, Button, Container } from 'semantic-ui-react'
-import { useInformationContext } from '../GatherInformation/utils/InformationState'
-import Loader from '../../components/Loader/index'
-
-export default function AllForm() {
-
-    const [informationState, informationDispatch] = useInformationContext();
-    const [state, dispatch] = useActiveTradingContext();
-    const [invalid, setInvalid] = useState(false);
-    const [buyLow, setBuylow] = useState();
-    const [buyHigh, setBuyhigh] = useState();
-    const [sellLow, setSelllow] = useState();
-    const [sellHigh, setSellhigh] = useState();
-
-    const validator = () => {
-        if ((Helper.verifyDrop(sellLow)) && (Helper.verifyDrop(buyLow)) && (Helper.verifyIncrease(buyHigh)) && (Helper.verifyIncrease(sellHigh))) {
-            return true
-        } return false
-    }
-
-    const handleSubmit = event => {
-        dispatch({ type: SET_PARAMS, buyLow: buyLow, buyHigh: buyHigh, sellLow: sellLow, sellHigh: sellHigh })
-        // API call 
-
-        const startDate = Helper.findFirstDateInYear(informationState.history, informationState.startYear)
-        const endDate = Helper.findLastDateInYear(informationState.history)
-
-        // start, end, symbol, BL, BH, SL, SH
-        API.runActiveTrading(startDate, endDate, informationState.symbol, buyLow, buyHigh, sellLow, sellHigh)
-        // check this. 
-          .then(res => {
-             API.runMultipleSimulations([
-               [informationState.symbol, startDate, endDate, informationState.investment, "activeTrading", [res]],
-               [informationState.symbol, startDate, endDate, informationState.investment, "buyAndWait", []],
-      
-               ])
-               .then(res => {
-                console.log(res)
-                informationDispatch({ type: SET_SIMULATION_DATA, data: res })
-               })
-          })
-        //
-
-        // setState .... 
-    }
-
-    if (!informationState.informationGathered) {
-        return null;
-    } else if (!state.buyLow) {
+import React from 'react'
+import { ActiveTradingProvider }from './utils/ActiveState'
+import AllForm from './Pages'
 
 
+function ActiveTrading () {
 
-
-        return (
-
-            <div>
-                <Segment fluid>
-
-                    <p>
-                        This is the buy low
-                <span>
-                            <Input size="mini" placeholder="buyLow" value={buyLow} onChange={(event, { value }) => setBuylow(value)} />
-                        </span>
-                    </p>
-
-                    <p>
-                        This is the buy high
-                <span>
-                            <Input size="mini" placeholder="buyHigh" value={buyHigh} onChange={(event, { value }) => setBuyhigh(value)} />
-                        </span>
-                    </p>
-
-
-                    <p>
-                        This is sell high
-                <span>
-                            <Input size="mini" placeholder="sellHigh" value={sellHigh} onChange={(event, { value }) => setSellhigh(value)} />
-                        </span>
-                    </p>
-
-                    <p>
-                        This is sell low
-                <span>
-                            <Input size="mini" placeholder="sellLow" value={sellLow} onChange={(event, { value }) => setSellLow(value)} />
-                        </span>
-                    </p>
-
-                </Segment>
-
-                {validator() ? <Button onClick={handleSubmit}>Run Simulation</Button> : null}
-
-
-            </div>
-
-        )
-    }
-
-    else {
-       {!informationState.simulationData ? <Loader /> : <ChartHandler simulations={informationState.simulationData} labels={[informationState.symbol]} />}
-    }
+    return (
+        <ActiveTradingProvider>
+            <AllForm/>
+        </ActiveTradingProvider>
+    )
 
 }
+
+export default ActiveTrading
--- client/src/pages/ActiveTrading/utils/ActiveState.js
+++ client/src/pages/ActiveTrading/utils/ActiveState.js
@@ -1,24 +1,14 @@
-import React, {createContext, useContext, useReducer} from "react"
-import {  SET_PARAMS , CLEAR_DATA, } from '../utils/activeAction'
+import React, { createContext, useContext, useReducer } from "react"
+import { SET_PARAMS, CLEAR_DATA, } from '../utils/activeAction'
 
 const ActiveTradingContext = createContext()
-const {Provider} = ActiveTradingContext
+const { Provider } = ActiveTradingContext
 
 const reducer = (state, action) => {
     switch (action.type) {
         case SET_PARAMS:
             return { ...state, buyLow: action.buyLow, buyHigh: action.buyHigh, sellLow: action.sellLow, sellHigh: action.sellHigh }
-           
-        // case SET_BUYHIGH:
-        //     return { ...state, buyHigh: action.buyHigh }
-          
-        // case SET_SELLLOW:
-        //     return { ...state, sellLow: action.sellLow }
-           
-        // case SET_SELLHIGH:
-        //     return { ...state, sellHigh: action.sellHigh }
-       
-       
+
         case CLEAR_DATA:
             return {
                 buyLow: null,
@@ -27,21 +17,21 @@ const reducer = (state, action) => {
                 sellHigh: null,
             }
             break
+    }
 }
-
-function ActiveTradingProvider({value=[], ...props}) {
-    const [state, dispatch] = useReducer(reducer, { 
+function ActiveTradingProvider({ value = [], ...props }) {
+    const [state, dispatch] = useReducer(reducer, {
         buyLow: null,
         buyHigh: null,
         sellLow: null,
         sellHigh: null,
     })
 
-    return <Provider value={[state, dispatch]} {...props}/>
+    return <Provider value={[state, dispatch]} {...props} />
 }
 
 function useActiveTradingContext() {
     return useContext(ActiveTradingContext)
 }
 
-export {ActiveTradingProvider, useActiveTradingContext}
+export { ActiveTradingProvider, useActiveTradingContext }
