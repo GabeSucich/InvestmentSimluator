7c62b767cc7e0a0df5a0993d0a3eded30f2db6d9
Gabe Sucich <gabe.sucich@gmail.com>
2020-08-31 11:22:45 -0700

    Changing mongoose connection for deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
 
-mongoose.connect(process.MONGODB_URI || "mongodb://localhost/stockhistorydb", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/stockhistorydb", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
 
 require("./routes/mainroutes")(app)
 require("./routes/userroutes")(app)
@@ -21,10 +21,6 @@ require("./routes/userroutes")(app)
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   })
-
-
-
-
 
 app.listen(PORT, err => {
     if (err) throw err
