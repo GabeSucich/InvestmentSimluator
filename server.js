2d004f0450caa7362a6f2652b0b99ee099358edc
Gabe Sucich <gabe.sucich@gmail.com>
2020-08-31 10:42:32 -0700

    Adding '/' route in deployment

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ app.use(express.json())
 
 app.use(express.static("public"))
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
 require("./routes/mainroutes")(app)
 require("./routes/userroutes")(app)
 
-app.use(function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   })
 
